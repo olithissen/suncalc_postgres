ce2d8ca032e5b2712b9a6ea4436ad78dad677453
Oli Thissen <oli@tonick.net>
2022-02-17 17:18:32 +0100

    Adds return for record type

--- suncalc/suncalc.c
+++ suncalc/suncalc.c
@@ -1,4 +1,5 @@
 #include "postgres.h"
+#include "funcapi.h"
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
@@ -321,4 +322,102 @@ Datum getSunPositionAltitude(PG_FUNCTION_ARGS)
     AzimuthAltitude az = getPosition(date, lat, lng);
 
     PG_RETURN_FLOAT8(az.altitude);
+}
+
+PG_FUNCTION_INFO_V1(getSunPosition);
+
+Datum getSunPosition(PG_FUNCTION_ARGS)
+{
+    FuncCallContext *funcctx;
+    int call_cntr;
+    int max_calls;
+    TupleDesc tupdesc;
+    AttInMetadata *attinmeta;
+
+    printf("Foobar");
+
+    /* stuff done only on the first call of the function */
+    if (SRF_IS_FIRSTCALL())
+    {
+        MemoryContext oldcontext;
+
+        /* create a function context for cross-call persistence */
+        funcctx = SRF_FIRSTCALL_INIT();
+
+        /* switch to memory context appropriate for multiple function calls */
+        oldcontext = MemoryContextSwitchTo(funcctx->multi_call_memory_ctx);
+
+        /* total number of tuples to be returned */
+        funcctx->max_calls = 1;
+
+        /* Build a tuple descriptor for our result type */
+        if (get_call_result_type(fcinfo, NULL, &tupdesc) != TYPEFUNC_COMPOSITE)
+            ereport(ERROR,
+                    (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+                     errmsg("function returning record called in context "
+                            "that cannot accept type record")));
+
+        /*
+         * generate attribute metadata needed later to produce tuples from raw
+         * C strings
+         */
+        attinmeta = TupleDescGetAttInMetadata(tupdesc);
+        funcctx->attinmeta = attinmeta;
+
+        MemoryContextSwitchTo(oldcontext);
+    }
+
+    /* stuff done on every call of the function */
+    funcctx = SRF_PERCALL_SETUP();
+
+    call_cntr = funcctx->call_cntr;
+    max_calls = funcctx->max_calls;
+    attinmeta = funcctx->attinmeta;
+
+    if (call_cntr < max_calls) /* do when there is more left to send */
+    {
+        char **values;
+        HeapTuple tuple;
+        Datum result;
+
+        float8 date;
+        float8 lat;
+        float8 lng;
+        AzimuthAltitude az;
+
+        /*
+         * Prepare a values array for building the returned tuple.
+         * This should be an array of C strings which will
+         * be processed later by the type input functions.
+         */
+        values = (char **)palloc(2 * sizeof(char *));
+        values[0] = (char *)palloc(16 * sizeof(char));
+        values[1] = (char *)palloc(16 * sizeof(char));
+
+        date= PG_GETARG_FLOAT8(0);
+        lat = PG_GETARG_FLOAT8(1);
+        lng = PG_GETARG_FLOAT8(2);
+        
+        az = getPosition(date, lat, lng);
+
+        snprintf(values[0], 16, "%f", az.azimuth);
+        snprintf(values[1], 16, "%f", az.altitude);
+
+        /* build a tuple */
+        tuple = BuildTupleFromCStrings(attinmeta, values);
+
+        /* make the tuple into a datum */
+        result = HeapTupleGetDatum(tuple);
+
+        /* clean up (this is not really necessary) */
+        pfree(values[0]);
+        pfree(values[1]);
+        pfree(values);
+
+        SRF_RETURN_NEXT(funcctx, result);
+    }
+    else /* do when there is no more left */
+    {
+        SRF_RETURN_DONE(funcctx);
+    }
 }
