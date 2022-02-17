028891c0b84711cd6d7e41951a226957631aeab9
Oli Thissen <oli@tonick.net>
2022-02-17 17:20:54 +0100

    gitignore and sql

--- suncalc/suncalc.sql
+++ suncalc/suncalc.sql
@@ -11,3 +11,10 @@ AS
 'getSunPositionAltitude'
     LANGUAGE C immutable
                strict;
+
+CREATE FUNCTION sun_position(double precision, double precision, double precision, out azimuth double precisionm, altitude double precision) RETURNS double precision
+AS
+'$libdir/suncalc.so',
+'getSunPosition'
+    LANGUAGE C immutable
+               strict;
